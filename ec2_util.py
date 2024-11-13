6943a22d7856c49fddbdf1e374c00a9f72ea86a7
lashmanbala <lashmanbala@gmail.com>
2024-11-13 10:07:53 +0530

    Updated with env variable

--- ec2_util.py
+++ ec2_util.py
@@ -1,8 +1,10 @@
 import boto3
 import logging
+import os
+import time
 
 logging.basicConfig(
-    filename="/home/bala/code/projects/redshift_project/app.log",
+    filename=os.environ.get('LOG_FILE_PATH'),
     filemode="w",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
@@ -16,6 +18,8 @@ def allocate_elastic_ip():
     try:
         response = ec2_client.allocate_address(Domain='vpc')
         logger.info('ElasticIP is created Successfully: %s', response['PublicIp'])
+
+        time.sleep(5) # wait untill its allocated
         return response
     
     except Exception as e:
