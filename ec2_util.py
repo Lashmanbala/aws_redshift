f9a780e713aa8fb9a858d07944bba90655c182e5
lashmanbala <lashmanbala@gmail.com>
2024-11-14 08:34:13 +0530

    Modified logging

--- ec2_util.py
+++ ec2_util.py
@@ -1,16 +1,7 @@
 import boto3
-import logging
-import os
 import time
+from logging_config import logger
 
-logging.basicConfig(
-    filename=os.environ.get('LOG_FILE_PATH'),
-    filemode="w",
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
 
 def allocate_elastic_ip():
     ec2_client = boto3.client('ec2')
