874ff01f65541faf427aee6ffc66f75a919d3658
lashmanbala <lashmanbala@gmail.com>
2024-12-03 18:45:52 +0530

    Modifying security group

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from ec2_util import allocate_elastic_ip
 from iam_util import create_iam_role
-from secrets_util import create_redshift_secret, get_kms_key_by_alias, get_secret
-from redshift_util import _create_cluster
+from secrets_util import create_redshift_secret, get_secret
+from redshift_util import _create_cluster, add_ip_to_redshift_security_group
 import dotenv
 import os
 
@@ -17,13 +17,18 @@ dotenv.load_dotenv()
 
 # role_arn = create_iam_role()
 
-secret = get_secret()
-user_name = secret['username']
-password = secret['password']
+# secret = get_secret()
+# user_name = secret['username']
+# password = secret['password']
 
-elasticIP = '54.174.124.10'
-iam_role_arn = 'arn:aws:iam::585768170668:role/Redshift_All_Commands_Access_Role'
-res = _create_cluster(elasticIP, iam_role_arn, user_name, password)
+cluster_identifier = 'retail-cluster'
+# elasticIP = '54.174.124.10'
+# iam_role_arn = 'arn:aws:iam::585768170668:role/Redshift_All_Commands_Access_Role'
+# res = _create_cluster(cluster_identifier, elasticIP, iam_role_arn, user_name, password)
+# print(res)
+# security_group_id = res['Cluster']['VpcSecurityGroups'][0]['VpcSecurityGroupId']
+
+security_group_id = 'sg-03b4db065615f216b'
+ip_address = os.environ.get('IP_ADDRESS')
+res = add_ip_to_redshift_security_group(security_group_id, ip_address)
 print(res)
-
-
--- redshift_util.py
+++ redshift_util.py
@@ -2,13 +2,13 @@ import boto3
 import time
 from logging_config import logger
 
-def _create_cluster(elasticIP, iam_role_arn, username, password ):
+def _create_cluster(cluster_identifier,elasticIP, iam_role_arn, username, password ):
 
     redshift_client = boto3.client('redshift')
 
     try:
         response = redshift_client.create_cluster(
-                ClusterIdentifier='retail-cluster',
+                ClusterIdentifier=cluster_identifier,
                 ClusterType='single-node',
                 NodeType='dc2.large',
                 MasterUsername=username,
@@ -26,3 +26,27 @@ def _create_cluster(elasticIP, iam_role_arn, username, password ):
     
     except Exception as e:
         logger.info(f'Error creating the cluster: {e}')
+
+
+def add_ip_to_redshift_security_group(security_group_id, ip_address):
+
+    ec2_client = boto3.client('ec2')
+    
+    try:
+        cidr_ip = f"{ip_address}/32"  # Specify a single IP address
+        
+        security_group_id = security_group_id
+
+        # Add inbound rule to allow access from the current IP
+        response = ec2_client.authorize_security_group_ingress(
+                                GroupId=security_group_id,
+                                IpProtocol='tcp',
+                                FromPort=5439,  # Default Redshift port
+                                ToPort=5439,
+                                CidrIp=cidr_ip
+                            )
+        logger.info(f"Successfully added inbound rule for IP {cidr_ip} to security group {security_group_id}.")
+        return response
+
+    except Exception as e:
+        logger.info(f"An unexpected error occurred: {e}")
