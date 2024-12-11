9e2f5aa48c791e174ca84fa90dabbbcab34c77cb
lashmanbala <lashmanbala@gmail.com>
2024-12-11 13:15:58 +0530

    Updating inbound rule

--- ec2_util.py
+++ ec2_util.py
@@ -15,3 +15,26 @@ def allocate_elastic_ip():
     
     except Exception as e:
         logger.error('Failed to allocate ElasticIP: %s', e)
+
+
+def add_ip_to_security_group(security_group_id, ip_address, port):
+
+    ec2_client = boto3.client('ec2')
+    
+    try:
+        cidr_ip = f"{ip_address}/32"  
+        
+        security_group_id = security_group_id
+
+        response = ec2_client.authorize_security_group_ingress(
+                                GroupId=security_group_id,
+                                IpProtocol='tcp',
+                                FromPort=port,  
+                                ToPort=port,
+                                CidrIp=cidr_ip
+                            )
+        logger.info(f"Successfully added inbound rule for IP {cidr_ip} to security group {security_group_id}.")
+        return response
+
+    except Exception as e:
+        logger.info(f"An unexpected error occurred: {e}")
